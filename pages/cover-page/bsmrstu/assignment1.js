5ec46ddb4db5c0d92c8a6a84f277bf597bf4c199
basir5101 <basir.bsmrstu@gmail.com>
2023-08-08 18:56:18 +0600

    done

--- pages/cover-page/bsmrstu/assignment1.js
+++ pages/cover-page/bsmrstu/assignment1.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Assignment from '@/components/pdf/Assignment';
 import dynamic from 'next/dynamic';
-// Use dynamic import to conditionally load the PDFViewer component
 const PDFViewer = dynamic(() => import('@react-pdf/renderer').then(mod => mod.PDFViewer), {
+    ssr: false, // Disable server-side rendering for PDFViewer
+});
+const PDFDownloadLink = dynamic(() => import('@react-pdf/renderer').then(mod => mod.PDFDownloadLink), {
     ssr: false, // Disable server-side rendering for PDFViewer
 });
 
 export default function assignment1() {
-
     const styles = {
         pdfContainer: {
             display: 'flex',
@@ -22,10 +23,15 @@ export default function assignment1() {
         },
     };
     return (
-        <div style={styles.pdfContainer}>
-            <PDFViewer style={styles.pdfViewer}>
-                <Assignment />
-            </PDFViewer>
-        </div>
+        <section className='container py-5'>
+            <PDFDownloadLink style={{ color: '#fff', borderRadius: '5px', backgroundColor: '#de33aa', padding: '5px 20px', textDecoration: 'none', }} document={<Assignment />} fileName="fee_acceptance.pdf">
+                {({ blob, url, loading, error }) => (loading ? 'Loading document...' : 'Download PDF')}
+            </PDFDownloadLink>
+            <div style={styles.pdfContainer}>
+                <PDFViewer style={styles.pdfViewer}>
+                    <Assignment />
+                </PDFViewer>
+            </div>
+        </section>
     )
 }
